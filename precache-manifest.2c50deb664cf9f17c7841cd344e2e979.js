51833dc136f924e2084a5564769b92caecf50443
8r4hm <brahmharsh24@gmail.com>
2019-05-01 06:33:36 +0530

    Updates

---
+++ precache-manifest.2c50deb664cf9f17c7841cd344e2e979.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "ddce3a1a5276f68740e3",
+    "url": "/portfolio/static/js/main.ddce3a1a.chunk.js"
+  },
+  {
+    "revision": "e990ccfaedfecdf9af24",
+    "url": "/portfolio/static/js/1.e990ccfa.chunk.js"
+  },
+  {
+    "revision": "ddce3a1a5276f68740e3",
+    "url": "/portfolio/static/css/main.63fb3f02.chunk.css"
+  },
+  {
+    "revision": "2db8fff051680446af158d4704c5c167",
+    "url": "/portfolio/index.html"
+  }
+];
